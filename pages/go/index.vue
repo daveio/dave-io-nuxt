cf014fe1e79496350410e4ff4cb59317122a39fc
Dave Williams <dave@dave.io>
2025-05-30 17:31:23 +0100

    🚚 rename Dennis Nedry GIF file to a shorter name
    Renamed the GIF file from "dennis-nedry.gif" to "nedry.gif" for a cleaner URL path and simplified the alt text for better accessibility. The shorter filename maintains the same context while reducing unnecessary verbosity in both the file reference and its description.

--- pages/go/index.vue
+++ pages/go/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="min-h-screen flex items-center justify-center bg-black">
     <div class="text-center">
-      <img 
-        src="/dennis-nedry.gif" 
-        alt="Dennis Nedry saying 'You didn't say the magic word'"
+      <img
+        src="/nedry.gif"
+        alt="You didn't say the magic word"
         class="mx-auto max-w-lg w-full h-auto"
       />
       <p class="text-green-400 font-mono text-lg mt-6">
